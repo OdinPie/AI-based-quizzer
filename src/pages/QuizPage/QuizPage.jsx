41be8f4798814b67669b7d7717cd6fe1ce7467cf
Maliha <maliha16tabassum@gmail.com>
2024-08-28 03:07:18 +0600

    answer checker function added

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -4,12 +4,26 @@ import Quiz from './Quiz';
 const QuizPage = () => {
     const [data, setData] = useState();
     const [index, setIndex] = useState(0);
-    const [question, setQuestion] = useState(data[index]);
+    const [question, setQuestion] = useState({});
+    const [result, setResult] = useState(false);
+    const [correct, setCorrect] = useState(true);
     useEffect(()=>{
         fetch('http://localhost:5000/questions')
         .then(res => res.json())
-        .then(data => setData(data))
+        .then(data => {
+            setData(data);
+            setQuestion(data[index]);
+        })
     },[])
+
+    const checkAnswer = (lock) =>{
+        setResult(true);
+        let status = true;
+        if(question.answer != lock){
+            status = false;
+            setCorrect(false);
+        }
+    }
     return (
         <div>
             {!data? 
@@ -18,9 +32,28 @@ const QuizPage = () => {
                 <h1 className='text-center heading'>Please Wait ...</h1>
             </div> 
             :
-            <div className='max-w-6xl mx-auto'>
-                
-            </div>} 
+            <div className='max-w-3xl mx-auto pt-32 paragraph'>
+                <h1 className='font-bold text-xl'>{index+1}. {question.question}</h1><br />
+                <ol className='a font-medium'>
+                    <li onClick={()=>{checkAnswer(1)}} id='1' className='q'>{question.options[0]}</li>
+                    <li onClick={()=>{checkAnswer(2)}} id='2' className='q'>{question.options[1]}</li>
+                    <li onClick={()=>{checkAnswer(3)}} id='3' className='q'>{question.options[2]}</li>
+                    <li onClick={()=>{checkAnswer(4)}} id='4' className='q'>{question.options[3]}</li>
+                </ol>
+                {
+                    result && <div className='fixed bottom-0'>
+                        {correct? 
+                        <div className='bg-[#CDFADB]'>
+                            
+                        </div> :
+                        <div className='bg-[#FCAEAE]'>
+
+                        </div>}
+                    </div>
+                }
+            </div>
+            
+            } 
         </div>
     );
 };
