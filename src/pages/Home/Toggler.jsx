975c33e9fc7b07a1aceff1a6497bf1a90e8773c2
Maliha <maliha16tabassum@gmail.com>
2024-08-27 19:15:30 +0600

    upload file function

--- src/pages/Home/Toggler.jsx
+++ src/pages/Home/Toggler.jsx
@@ -5,14 +5,35 @@ const Toggler = () => {
     const [text, setText] = useState(true);
     const [uploaded, setUploaded] = useState(false);
     const [filename, setfileName] = useState("");
-    const fileHandler = (e) =>{
+    const [filesize, setfileSize] = useState(0);
+    const fileHandler = () =>{
         const file = document.getElementById('files').files[0];
         const filename = file.name;
         const filesize = file.size;
         setfileName(filename);
-        setUploaded(true);
-        
+        setfileSize(filesize);
+        setUploaded(true);  
     }
+
+    const uploadFile = () =>{
+        const file = document.getElementById('files').files[0];
+        const data = {file, filename, filesize};
+
+        fetch('http://localhost:5000/post-file',{
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: data
+        }).then(
+            res => res.json()
+        ).then(
+            success => console.log(success)  
+        ).catch(
+            error => console.log(error)  
+        )
+    }
+
     return (
         <div>
             <button onClick={()=>{setText(true)}} className="btn">Text</button>
@@ -27,14 +48,14 @@ const Toggler = () => {
             <div className='file-uploader'>
                 <div className='flex flex-col items-center relative'>
                 <input type="file" onChange={fileHandler} className="filer z-50 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 size-80" id='files' />
-                <div className='flex flex-col justify-center items-center w-1/2 p-20 my-10 border-2 border-dashed border-blue-600 bg-[#D1E9F6] relative'>
+                <div className='flex flex-col justify-center items-center w-1/2 p-20 my-10 rounded-lg border-2 border-dashed border-blue-600 bg-[#D1E9F6] relative'>
                     <MdUploadFile className='text-6xl animate-pulse'></MdUploadFile><br />
                     {
-                        !uploaded? <p>Drag & Drop or Upload Your Document Here</p> : <p>{filename}</p>
+                        !uploaded? <p>Drag & Drop or Upload Your Document Here</p> : <p className=''>{filename}</p>
                     }
                 </div>
                 </div>
-                <button className="btn">Upload</button>
+                <button onClick={uploadFile} className="btn">Upload</button>
                 
             </div>}
 
