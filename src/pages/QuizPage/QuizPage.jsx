98daa5275c4b88b89627ac6fab43411fe8442a44
Maliha <maliha16tabassum@gmail.com>
2024-08-28 15:26:48 +0600

    question flow works

--- src/pages/QuizPage/QuizPage.jsx
+++ src/pages/QuizPage/QuizPage.jsx
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
 import Quiz from './Quiz';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleCheck } from '@fortawesome/free-solid-svg-icons'
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons'
 const QuizPage = () => {
-    const [data, setData] = useState();
-    const [index, setIndex] = useState(0);
-    const [question, setQuestion] = useState({});
-    const [result, setResult] = useState(false);
-    const [correct, setCorrect] = useState(true);
+    let [data, setData] = useState();
+    let [index, setIndex] = useState(0);
+    let [question, setQuestion] = useState({});
+    let [result, setResult] = useState(false);
+    let [correct, setCorrect] = useState(true);
+    let [lock, setLock] = useState(false);
     useEffect(()=>{
         fetch('http://localhost:5000/questions')
         .then(res => res.json())
@@ -16,14 +19,30 @@ const QuizPage = () => {
         })
     },[])
 
-    const checkAnswer = (lock) =>{
-        setResult(true);
+    const checkAnswer = (selected) =>{
+        if(!lock){
+            setResult(true);
         let status = true;
-        if(question.answer != lock){
+        let option = document.getElementById(selected);
+        option.style.backgroundColor = '#F6EACB';
+        setLock(true);
+        if(question.answer != selected){
             status = false;
             setCorrect(false);
+            // console.log("wrong!!!")
+        }else{
+            // console.log("correct!!!")
+
+        }
         }
     }
+
+    const nextQuestion = () =>{
+        setIndex(++index);
+        setQuestion(data[index]);
+        setLock(false);
+    }
+
     return (
         <div>
             {!data? 
@@ -32,21 +51,41 @@ const QuizPage = () => {
                 <h1 className='text-center heading'>Please Wait ...</h1>
             </div> 
             :
-            <div className='max-w-3xl mx-auto pt-32 paragraph'>
-                <h1 className='font-bold text-xl'>{index+1}. {question.question}</h1><br />
-                <ol className='a font-medium'>
-                    <li onClick={()=>{checkAnswer(1)}} id='1' className='q'>{question.options[0]}</li>
-                    <li onClick={()=>{checkAnswer(2)}} id='2' className='q'>{question.options[1]}</li>
-                    <li onClick={()=>{checkAnswer(3)}} id='3' className='q'>{question.options[2]}</li>
-                    <li onClick={()=>{checkAnswer(4)}} id='4' className='q'>{question.options[3]}</li>
-                </ol>
+            <div className='pt-32 paragraph'>
+                <div className='max-w-3xl mx-auto '>
+                    <h1 className='font-bold text-xl'>{index+1}. {question.question}</h1><br />
+                    <ol className='a font-medium'>
+                        <li onClick={()=>{checkAnswer(1)}} id='1' className='q'>{question.options[0]}</li>
+                        <li onClick={()=>{checkAnswer(2)}} id='2' className='q'>{question.options[1]}</li>
+                        <li onClick={()=>{checkAnswer(3)}} id='3' className='q'>{question.options[2]}</li>
+                        <li onClick={()=>{checkAnswer(4)}} id='4' className='q'>{question.options[3]}</li>
+                    </ol>
+                </div>
                 {
-                    result && <div className='fixed bottom-0'>
+                    result && <div className='fixed bottom-0 w-full'>
                         {correct? 
-                        <div className='bg-[#CDFADB]'>
+                        <div className='bg-[#DCFFB7] flex items-center justify-center gap-52'>
+                            <div className='flex items-center py-2'>
+                                <FontAwesomeIcon icon={faCircleCheck} className='text-7xl' style={{color: "#9ADE7B",}} />
+                                <h1 className='paragraph ml-5 font-bold text-green-800'>Correct</h1>
+                            </div>
+                            <div>
+                                <button onClick={nextQuestion} className="btn rounded-full w-20 bg-[#9ADE7B] text-white">Next</button>
+                            </div>
                             
                         </div> :
-                        <div className='bg-[#FCAEAE]'>
+                        <div className='bg-[#FCAEAE] flex items-center justify-center gap-52'>
+                            <div className='flex items-center py-2'>
+                                <FontAwesomeIcon icon={faCircleXmark} className='text-7xl' style={{color: "#FF8080"}}/>
+                                <div>
+                                <h1 className='paragraph ml-5 font-bold text-red-500'>Correct Answer:</h1>
+                                <p className='paragraph ml-5 text-red-500'>{question.options[question.answer-1]}</p>
+                                </div>
+                                
+                            </div>
+                            <div>
+                                <button onClick={nextQuestion} className="btn rounded-full w-20 bg-[#FF8080] text-white">Next</button>
+                            </div>
 
                         </div>}
                     </div>
