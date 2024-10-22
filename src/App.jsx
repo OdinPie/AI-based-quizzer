8939c4d55f65fcdb27191691340667bac47d9549
Maliha <maliha16tabassum@gmail.com>
2024-10-22 19:23:45 +0600

    fixed navbar link

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './shared/navbar/navbar'
 import { Outlet, useLocation } from 'react-router-dom'
- 
+import Navbar from './shared/navbar/Navbar'
 function App() {
   const [count, setCount] = useState(0)
   const [noshoulder, setnoShoulder] = useState(false);
